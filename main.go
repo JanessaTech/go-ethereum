c5bdf3c490fccc3897c8bc7697a5cbe3eacbf4de
Jane <lvlajzhao1983@gamil.com>
2023-04-14 10:04:34 +0800

    add a demo on how to deploy smart contract using go

---
+++ main.go
@@ -0,0 +1,324 @@
+// Code generated - DO NOT EDIT.
+// This file is a generated binding and any manual changes will be lost.
+
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"math/big"
+	"strings"
+	"time"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/event"
+)
+
+// Reference imports to suppress errors if they are not otherwise used.
+var (
+	_ = errors.New
+	_ = big.NewInt
+	_ = strings.NewReader
+	_ = ethereum.NotFound
+	_ = bind.Bind
+	_ = common.Big1
+	_ = types.BloomLookup
+	_ = event.NewSubscription
+	_ = abi.ConvertType
+)
+
+// StorageMetaData contains all meta data concerning the Storage contract.
+var StorageMetaData = &bind.MetaData{
+	ABI: "[{\"inputs\":[],\"name\":\"retrieve\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"number\",\"type\":\"uint256\"}],\"name\":\"set\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]",
+	Bin: "0x6080604052600560005534801561001557600080fd5b50610150806100256000396000f3fe608060405234801561001057600080fd5b50600436106100365760003560e01c80632e64cec11461003b57806360fe47b114610059575b600080fd5b610043610075565b60405161005091906100a1565b60405180910390f35b610073600480360381019061006e91906100ed565b61007e565b005b60008054905090565b8060008190555050565b6000819050919050565b61009b81610088565b82525050565b60006020820190506100b66000830184610092565b92915050565b600080fd5b6100ca81610088565b81146100d557600080fd5b50565b6000813590506100e7816100c1565b92915050565b600060208284031215610103576101026100bc565b5b6000610111848285016100d8565b9150509291505056fea264697066735822122090bacae41ca9f97ce84cb7f9082628fa06fce961bb51718494caccb7191cba0b64736f6c63430008130033",
+}
+
+// StorageABI is the input ABI used to generate the binding from.
+// Deprecated: Use StorageMetaData.ABI instead.
+var StorageABI = StorageMetaData.ABI
+
+// StorageBin is the compiled bytecode used for deploying new contracts.
+// Deprecated: Use StorageMetaData.Bin instead.
+var StorageBin = StorageMetaData.Bin
+
+// DeployStorage deploys a new Ethereum contract, binding an instance of Storage to it.
+func DeployStorage(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *Storage, error) {
+	parsed, err := StorageMetaData.GetAbi()
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	if parsed == nil {
+		return common.Address{}, nil, nil, errors.New("GetABI returned nil")
+	}
+
+	address, tx, contract, err := bind.DeployContract(auth, *parsed, common.FromHex(StorageBin), backend)
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	return address, tx, &Storage{StorageCaller: StorageCaller{contract: contract}, StorageTransactor: StorageTransactor{contract: contract}, StorageFilterer: StorageFilterer{contract: contract}}, nil
+}
+
+// Storage is an auto generated Go binding around an Ethereum contract.
+type Storage struct {
+	StorageCaller     // Read-only binding to the contract
+	StorageTransactor // Write-only binding to the contract
+	StorageFilterer   // Log filterer for contract events
+}
+
+// StorageCaller is an auto generated read-only Go binding around an Ethereum contract.
+type StorageCaller struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// StorageTransactor is an auto generated write-only Go binding around an Ethereum contract.
+type StorageTransactor struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// StorageFilterer is an auto generated log filtering Go binding around an Ethereum contract events.
+type StorageFilterer struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// StorageSession is an auto generated Go binding around an Ethereum contract,
+// with pre-set call and transact options.
+type StorageSession struct {
+	Contract     *Storage          // Generic contract binding to set the session for
+	CallOpts     bind.CallOpts     // Call options to use throughout this session
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// StorageCallerSession is an auto generated read-only Go binding around an Ethereum contract,
+// with pre-set call options.
+type StorageCallerSession struct {
+	Contract *StorageCaller // Generic contract caller binding to set the session for
+	CallOpts bind.CallOpts  // Call options to use throughout this session
+}
+
+// StorageTransactorSession is an auto generated write-only Go binding around an Ethereum contract,
+// with pre-set transact options.
+type StorageTransactorSession struct {
+	Contract     *StorageTransactor // Generic contract transactor binding to set the session for
+	TransactOpts bind.TransactOpts  // Transaction auth options to use throughout this session
+}
+
+// StorageRaw is an auto generated low-level Go binding around an Ethereum contract.
+type StorageRaw struct {
+	Contract *Storage // Generic contract binding to access the raw methods on
+}
+
+// StorageCallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
+type StorageCallerRaw struct {
+	Contract *StorageCaller // Generic read-only contract binding to access the raw methods on
+}
+
+// StorageTransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
+type StorageTransactorRaw struct {
+	Contract *StorageTransactor // Generic write-only contract binding to access the raw methods on
+}
+
+// NewStorage creates a new instance of Storage, bound to a specific deployed contract.
+func NewStorage(address common.Address, backend bind.ContractBackend) (*Storage, error) {
+	contract, err := bindStorage(address, backend, backend, backend)
+	if err != nil {
+		return nil, err
+	}
+	return &Storage{StorageCaller: StorageCaller{contract: contract}, StorageTransactor: StorageTransactor{contract: contract}, StorageFilterer: StorageFilterer{contract: contract}}, nil
+}
+
+// NewStorageCaller creates a new read-only instance of Storage, bound to a specific deployed contract.
+func NewStorageCaller(address common.Address, caller bind.ContractCaller) (*StorageCaller, error) {
+	contract, err := bindStorage(address, caller, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &StorageCaller{contract: contract}, nil
+}
+
+// NewStorageTransactor creates a new write-only instance of Storage, bound to a specific deployed contract.
+func NewStorageTransactor(address common.Address, transactor bind.ContractTransactor) (*StorageTransactor, error) {
+	contract, err := bindStorage(address, nil, transactor, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &StorageTransactor{contract: contract}, nil
+}
+
+// NewStorageFilterer creates a new log filterer instance of Storage, bound to a specific deployed contract.
+func NewStorageFilterer(address common.Address, filterer bind.ContractFilterer) (*StorageFilterer, error) {
+	contract, err := bindStorage(address, nil, nil, filterer)
+	if err != nil {
+		return nil, err
+	}
+	return &StorageFilterer{contract: contract}, nil
+}
+
+// bindStorage binds a generic wrapper to an already deployed contract.
+func bindStorage(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
+	parsed, err := StorageMetaData.GetAbi()
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Storage *StorageRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
+	return _Storage.Contract.StorageCaller.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Storage *StorageRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Storage.Contract.StorageTransactor.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Storage *StorageRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Storage.Contract.StorageTransactor.contract.Transact(opts, method, params...)
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Storage *StorageCallerRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
+	return _Storage.Contract.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Storage *StorageTransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Storage.Contract.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Storage *StorageTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Storage.Contract.contract.Transact(opts, method, params...)
+}
+
+// Retrieve is a free data retrieval call binding the contract method 0x2e64cec1.
+//
+// Solidity: function retrieve() view returns(uint256)
+func (_Storage *StorageCaller) Retrieve(opts *bind.CallOpts) (*big.Int, error) {
+	var out []interface{}
+	err := _Storage.contract.Call(opts, &out, "retrieve")
+
+	if err != nil {
+		return *new(*big.Int), err
+	}
+
+	out0 := *abi.ConvertType(out[0], new(*big.Int)).(**big.Int)
+
+	return out0, err
+
+}
+
+// Retrieve is a free data retrieval call binding the contract method 0x2e64cec1.
+//
+// Solidity: function retrieve() view returns(uint256)
+func (_Storage *StorageSession) Retrieve() (*big.Int, error) {
+	return _Storage.Contract.Retrieve(&_Storage.CallOpts)
+}
+
+// Retrieve is a free data retrieval call binding the contract method 0x2e64cec1.
+//
+// Solidity: function retrieve() view returns(uint256)
+func (_Storage *StorageCallerSession) Retrieve() (*big.Int, error) {
+	return _Storage.Contract.Retrieve(&_Storage.CallOpts)
+}
+
+// Set is a paid mutator transaction binding the contract method 0x60fe47b1.
+//
+// Solidity: function set(uint256 number) returns()
+func (_Storage *StorageTransactor) Set(opts *bind.TransactOpts, number *big.Int) (*types.Transaction, error) {
+	return _Storage.contract.Transact(opts, "set", number)
+}
+
+// Set is a paid mutator transaction binding the contract method 0x60fe47b1.
+//
+// Solidity: function set(uint256 number) returns()
+func (_Storage *StorageSession) Set(number *big.Int) (*types.Transaction, error) {
+	return _Storage.Contract.Set(&_Storage.TransactOpts, number)
+}
+
+// Set is a paid mutator transaction binding the contract method 0x60fe47b1.
+//
+// Solidity: function set(uint256 number) returns()
+func (_Storage *StorageTransactorSession) Set(number *big.Int) (*types.Transaction, error) {
+	return _Storage.Contract.Set(&_Storage.TransactOpts, number)
+}
+
+func deployContract() {
+	const key = `{"address":"97dfe2541fd011b67fde4eb6956977f18b2a44c9","crypto":{"cipher":"aes-128-ctr","ciphertext":"1319f26457d2e614117b30c0b010b569a89c2b955af766b7d439502ccab01699","cipherparams":{"iv":"3dfde7bb7163a6d60516010287afc40b"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":4096,"p":6,"r":8,"salt":"53b2b24e524710e53d6f49ff6129985bc6a5dc41d4358851c51f5caa39296f41"},"mac":"664153009dc9497a00fd0c61591a303c3fb748fa515a7a6c3dd9c7640648246a"},"id":"afe89208-ab0c-4095-8771-669a427294d7","version":3}`
+
+	cl, err := ethclient.Dial("http://127.0.0.1:8545")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("cl is created")
+	_ = cl
+
+	chainId, err := cl.ChainID(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	auth, err := bind.NewTransactorWithChainID(strings.NewReader(key), "2JYhuVMW6pW9dQJvugNY6d6YkHkqp3pY", chainId)
+	if err != nil {
+		panic(err)
+	}
+
+	address, tx, instance, err := DeployStorage(auth, cl)
+	if err != nil {
+		log.Fatalf("Failed to deploy new storage contract: %v", err)
+	}
+	fmt.Printf("Contract pending deploy: 0x%x\n", address)
+	fmt.Printf("Transaction waiting to be mined: 0x%x\n\n", tx.Hash())
+
+	time.Sleep(250 * time.Millisecond) // Allow it to be processed by the local node :P
+
+	// function call on `instance`. Retrieves pending name
+	name, err := instance.Retrieve(&bind.CallOpts{Pending: true})
+	if err != nil {
+		log.Fatalf("Failed to retrieve pending name: %v", err)
+	}
+	fmt.Println("Pending name:", name)
+}
+
+func callRetrieve() {
+	cl, err := ethclient.Dial("http://127.0.0.1:8545")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("cl is created")
+
+	store, err := NewStorage(common.HexToAddress("0xF7F23920469fe375F812B098Cb3973b59A085C7d"), cl)
+	if err != nil {
+		log.Fatalf("Failed to instantiate Storage contract: %v", err)
+	}
+	value, err := store.Retrieve(nil)
+	if err != nil {
+	log.Fatalf("Failed to retrieve value: %v", err)
+	}
+	fmt.Println("Value: ", value)
+}
+
+
+func main() {
+	fmt.Println("start in main .....")
+	deployContract()
+	//callRetrieve()
+
+
+}
